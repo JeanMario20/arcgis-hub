a2569042420b9e3b5649dfb3d41412db91ec048e
JEANMARIO <jean.hernandez@sigsa.com.mx>
2025-07-24 07:13:21 -0600

    se hizo el boton de retroceder el problema no sigue el dibujo cuando se quiere seguir dibujando

--- src/components/bufferWidget/bufferWidget.tsx
+++ src/components/bufferWidget/bufferWidget.tsx
@@ -3,7 +3,7 @@ import {type ReactNode, type MouseEvent, useEffect, useRef, type RefObject, useS
 import { useMap } from '../../context/viewContext';
 import Graphic from "@arcgis/core/Graphic.js"
 import SimpleMarkerSymbol from "arcgis/core/symbols/SimpleMarkerSymbol"
-import type GraphicsLayer from "@arcgis/core/layers/GraphicsLayer";
+import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer";
 
 
 interface Props{
@@ -49,9 +49,9 @@ function BufferWidget(){
             })
             const handlerDoubleClick = view.current?.on("double-click",(event) => {
                 setIsDrawPolyline( prev => !prev);
-                console.log("se hizo doble click");
                 
             })
+            
             return () => {
                 handlerClick?.remove();
                 handlerDoubleClick?.remove();
@@ -61,6 +61,74 @@ function BufferWidget(){
         }
     
     }, [isDrawPolyline, clickRef, view])
+    
+
+    function borrarPolyline(){
+        bufferLayer.current?.graphics.removeAll();
+        
+    }
+
+    function retrocederDibujo(){
+        const graphics = bufferLayer.current?.graphics
+        const arrayCoords: number [][] = []
+        const polylineObjec = graphics?.find((layer) =>{
+            return layer.attributes.type == "polyline"
+        })
+
+        if(polylineObjec)
+        graphics?.remove(polylineObjec)
+    
+        if(graphics){
+            const limit = graphics.length - 1
+            
+
+            for(let i = 0; i < limit; i++){
+                const LayerInfo = graphics.getItemAt(i)?.attributes;
+                if(LayerInfo){
+                    arrayCoords.push([Number(LayerInfo.x), Number(LayerInfo.y)])
+                }
+            }
+        }
+
+        graphics?.pop()
+
+        const polyline = {
+            type: "polyline",
+            paths: [
+                arrayCoords
+            ]
+        };
+
+        /*const lineGeometry = new polyline({
+            hasZ: false,
+            hasM: true,
+            paths: polyline,
+            spatialReference: { wkid: 4326 }
+        });*/
+
+        const lineSymbol = {
+            type: "simple-line",
+            color: [226, 119, 40],
+            width: 4
+        };
+
+        const lineAtt = {
+            Name: "temp",
+            owner: "temp",
+            length: "temp",
+            type: "polyline",
+        };
+
+        const polylineGraphics = new Graphic({
+            geometry: polyline,
+            Symbol: lineSymbol,
+            attributes: lineAtt,
+        });
+        
+        graphics?.add(polylineGraphics);
+        
+        
+    }
 
     return(
     <>
@@ -70,7 +138,8 @@ function BufferWidget(){
             <Div>
                 <div id="buttonContainer">
                 <Button onClick={() => setIsDrawPolyline(prev => !prev)}>Polyline</Button>
-                <button>Borrar todo</button>
+                <Button onClick={borrarPolyline}>Borrar todo</Button>
+                <Button onClick={retrocederDibujo}>Retroceder</Button>
             </div>
             <div id="bufferOptionsForm">
                 <label id="bufferOptionsLabels" htmlFor="someText1">ejemplo</label>
@@ -170,7 +239,6 @@ function DibujarPolyline(view: RefObject<__esri.MapView | null>, event: __esri.V
             attributes: lineAtt,
         });
         
-        //view.current?.graphics.add(polylineGraphics);
         bufferLayer.current?.graphics.add(polylineGraphics);
     }
 
